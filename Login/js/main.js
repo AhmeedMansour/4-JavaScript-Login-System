57b9256cef35f2ddcc999aac725e52277586e541
AhmeedMansour <ahmedee.mansour@gmail.com>
2024-12-07 22:05:01 +0200

    inital commit

---
+++ Login/js/main.js
@@ -0,0 +1,52 @@
+let loginForm = document.getElementById("loginForm");
+let loginEmail = document.getElementById("loginEmail");
+let loginPassword = document.getElementById("loginPassword");
+let loginAlert = document.getElementById("loginAlert");
+let loginSuccessAlert = document.getElementById("loginSuccessAlert");
+let eyeIcon = document.getElementById("eyeIcon");
+let allUsers = [];
+
+if (localStorage.getItem("allUsers") !== null) {
+  allUsers = JSON.parse(localStorage.getItem("allUsers"));
+}
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  login();
+});
+
+function login() {
+  let userData = {
+    email: loginEmail.value,
+    password: loginPassword.value,
+  };
+  if (isLoginValid(userData)) {
+    console.log("Welcome!");
+    loginSuccessAlert.classList.replace("d-none", "d-block");
+    setTimeout(function () {
+      window.location.href = "../Welcome/index.html";      
+    }, 1500)
+  } else {
+    loginAlert.classList.replace("d-none", "d-block");
+    loginPassword.value = "";
+    setTimeout(function () {
+      loginAlert.classList.replace("d-block", "d-none");
+    }, 3000);
+  }
+}
+
+function isLoginValid(userData) {
+  for (let i = 0; i < allUsers.length; i++) {
+    if (
+      allUsers[i].email.toLowerCase() == userData.email.toLowerCase() &&
+      allUsers[i].password == userData.password
+    ) {
+      return true;
+    }
+  }
+}
+
+eyeIcon.addEventListener("click", function () {
+  loginPassword.type = loginPassword.type === "password" ? "text" : "password";
+  eyeIcon.classList.toggle("fa-eye-slash");
+});
---
+++ Sign Up/js/main.js
@@ -0,0 +1,97 @@
+/* 
+email regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+username regex = /^[a-zA-Z][a-zA-Z0-9_]{2,15}$/
+Password regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+*/
+
+let registerForm = document.getElementById("registerForm");
+let signName = document.getElementById("signName");
+let signEmail = document.getElementById("signEmail");
+let signPassword = document.getElementById("signPassword");
+let nameAlert = document.getElementById("nameAlert");
+let emailAlert = document.getElementById("emailAlert");
+let passwordAlert = document.getElementById("passwordAlert");
+let existAlert = document.getElementById("existAlert");
+let successAlert = document.getElementById("successAlert");
+let eyeIcon = document.getElementById("eyeIcon");
+
+let allUsers = [];
+
+if (localStorage.getItem("allUsers") !== null) {
+  allUsers = JSON.parse(localStorage.getItem("allUsers"));
+}
+
+registerForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (checkAllInputs()) {
+    addUser();
+  } else {
+  }
+});
+
+function addUser() {
+  let newUser = {
+    name: signName.value,
+    email: signEmail.value,
+    password: signPassword.value,
+  };
+
+  if (isExist(newUser) == true) {   
+    return true;
+  } else {
+    successAlert.classList.replace("d-none", "d-block");
+    existAlert.classList.replace("d-block", "d-none");
+    allUsers.push(newUser);
+    localStorage.setItem("allUsers", JSON.stringify(allUsers));
+
+    setTimeout(function () {
+        window.location.href = '../Login/index.html';
+    }, 1700)
+  }
+
+  function isExist(newUser) {
+    for (let i = 0; i < allUsers.length; i++) {
+      if (allUsers[i].email.toLowerCase() == newUser.email.toLowerCase()) {
+        existAlert.classList.replace("d-none", "d-block");
+        return true;
+      }
+    }
+  }
+}
+
+function validateAllInputs(regex, element, alertMsg) {
+  let pattern = regex;
+
+  if (pattern.test(element.value) == true) {
+    alertMsg.classList.replace("d-block", "d-none");
+    return true;
+  } else {
+    alertMsg.classList.replace("d-none", "d-block");
+    return false;
+  }
+}
+
+function checkAllInputs() {
+  if (
+    validateAllInputs(/^[a-zA-Z][a-zA-Z0-9_]{2,15}$/, signName, nameAlert) &&
+    validateAllInputs(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      signEmail,
+      emailAlert
+    ) &&
+    validateAllInputs(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      signPassword,
+      passwordAlert
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+eyeIcon.addEventListener("click", function () {
+  signPassword.type = signPassword.type === "password" ? "text" : "password";
+  eyeIcon.classList.toggle("fa-eye-slash");
+});
